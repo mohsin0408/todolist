e7f163fd5cd9d6be150bbb7c6ba551baeaae8c5d
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2024-08-12 15:36:57 +0530

    media query added

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
 import React, { useState } from "react";
 import ToDoList from "./ToDoList";
+import './index.css'; 
 
 const App = () => {
-  const [items,setItems] = useState([]);
-  const [inputList,setInputList] = useState('');
+  const [items, setItems] = useState([]);
+  const [inputList, setInputList] = useState('');
 
   const itemEvent = (event) => {
-    setInputList(event.target.value)
+    setInputList(event.target.value);
   };
 
-  function deleteItems(id) {
-    console.log('deleted')
-    setItems((oldItems)=>{return oldItems.filter((arrElem, index)=> {
-      return index !== id;
-    })
-  })
-  };
-  function listofItems(){  
-    setItems([...items,inputList]);
-    setInputList('');
+  const deleteItems = (id) => {
+    setItems((oldItems) => oldItems.filter((_, index) => index !== id));
   };
 
-  function HandleKeyDown (event){
-    if(event.key=== 'Enter'){
+  const listofItems = () => {
+    if (inputList.trim() !== "") { 
+      setItems([...items, inputList]);
+      setInputList('');
+    }
+  };
+
+  const HandleKeyDown = (event) => {
+    if (event.key === 'Enter') {
       listofItems();
     }
   };
 
   return (
-    <>
-      <div className="main-div">
-        <div className="center-div" >
-          <h1>ToDoList</h1>
-          <input type="text" placeholder="Add Items" value={inputList} onChange={itemEvent} onKeyDown={HandleKeyDown} />
-          <button onClick={listofItems}> + </button>
-          <ol>
-          {items.map ((itemval,index) => {
-            return (<ToDoList id={index} key={index} text={itemval} onSelect={deleteItems} />)
-          })}
-          </ol>
-        </div>
+    <div className="main-div">
+      <div className="center-div">
+        <h1>ToDoList</h1>
+        <input 
+          type="text" 
+          placeholder="Add Items" 
+          value={inputList} 
+          onChange={itemEvent} 
+          onKeyDown={HandleKeyDown} 
+        />
+        <button onClick={listofItems}> + </button>
+        <ol>
+          {items.map((itemval, index) => (
+            <ToDoList id={index} key={index} text={itemval} onSelect={deleteItems} />
+          ))}
+        </ol>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App;
